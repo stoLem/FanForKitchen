689eac3b8bddf523bdbe2a08cceb1e9550cb4fbe
Konstantin <chinyaev.kv@yandex.ru>
2020-07-21 20:18:50 +0400

    add Atmel

---
+++ FanForKitchen_Atmel/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * FanForKitchen.cpp
+ *
+ * Created: 21.07.2020 17:29:03
+ * Author : Константин
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+
+    }
+}
+
