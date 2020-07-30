6cb06303a9d771bbf8e282c15168242c172be1af
stoLem <stoLem@users.noreply.github.com>
2020-07-30 17:43:48 +0400

    Update main.ino

--- FanForKitchen_Digispark+Arduino/main/main.ino
+++ FanForKitchen_Digispark+Arduino/main/main.ino
@@ -1,62 +1,49 @@
-#define dimPin PB0
-#define zeroPin PB2
-#include <core_times.h>
-volatile int tic, Dimmer;
+#define dimPin 0
+#define btnSlowlyPin 1
+#define zeroPin 2
+#define btnFasterPin 3
+#define btnMinPin 4
+#define btnMaxPin 5
+#include <core_timers.h>
 
-digitalWriteF(uint8_t pin, uint8_t val);
+volatile uint8_t Dimmer; // 1 - 161
 
 void setup() {
   pinMode(dimPin, OUTPUT);
-  digitalWriteF(dimPin, 0);
+  digitalWrite(dimPin, LOW);
+
+  pinMode(btnSlowlyPin, INPUT_PULLUP);
+  pinMode(btnFasterPin, INPUT_PULLUP);
+  pinMode(btnMinPin, INPUT_PULLUP);
+  pinMode(btnMaxPin, INPUT_PULLUP);
+
   pinMode(zeroPin, INPUT);                 // настраиваем порт на вход для отслеживания прохождения сигнала через ноль
-  attachInterrupt(0, detect_up, FALLING);  // настроить срабатывание прерывания interrupt0 на pin 2 на низкий уровень
+  attachInterrupt(0, detect_up, RISING);   // настроить срабатывание прерывания на восходящий уровень
 
-  StartTimer1(timer_interrupt, 40);        // время для одного разряда ШИМ
-  StopTimer1();                            // остановить таймер
-
+  Dimmer = 50;
 }
 
 void loop() {
-  if (digitalRead(BTN1_PIN) == 0){
-    if (Dimmer != 99) Dimmer = 99;
-  } else if (digitalRead(BTN2_PIN) == 0){
-    if (Dimmer != 0) Dimmer = 0;
-  } else {
-    if (Dimmer != 85) Dimmer = 85;
-  }
+//  if (digitalRead(BTN1_PIN) == 0){
+//    if (Dimmer != 99) Dimmer = 99;
+//  } else if (digitalRead(BTN2_PIN) == 0){
+//    if (Dimmer != 0) Dimmer = 0;
+//  } else {
+//    if (Dimmer != 85) Dimmer = 85;
+//  }
 }
 
 //----------------------ОБРАБОТЧИКИ ПРЕРЫВАНИЙ--------------------------
-void timer_interrupt() {       // прерывания таймера срабатывают каждые 40 мкс
-  tic++;                       // счетчик
-  if (tic > Dimmer)            // если настало время включать ток
-    digitalWriteF(dimPin, 1)  
+void detect_up() {    // обработка внешнего прерывания на пересекание нуля снизу                                                        
+  digitalWrite(dimPin, LOW);
+  
+  Timer0_SetOutputCompareMatchA(Dimmer);        
+  Timer0_EnableOutputCompareInterruptA();
+  Timer0_ClockSelect(Timer0_Prescale_Value_1024);// запустить таймер
 }
 
-void  detect_up() {    // обработка внешнего прерывания на пересекание нуля снизу
-  tic = 0;                                  // обнулить счетчик
-  ResumeTimer1();                           // перезапустить таймер
-  attachInterrupt(0, detect_down, RISING);  // перенастроить прерывание
-}
-
-void  detect_down() {  // обработка внешнего прерывания на пересекание нуля сверху
-  tic = 0;                                  // обнулить счетчик
-  StopTimer1();                             // остановить таймер
-  digitalWriteF(dimPin, 0);                  // вырубить ток
-  attachInterrupt(0, detect_up, FALLING);   // перенастроить прерывание
+void timer_interrupt() {      
+  digitalWrite(dimPin, HIGH);
+  Timer0_SetToPowerup(); // остановить таймер
 }
 //----------------------ОБРАБОТЧИКИ ПРЕРЫВАНИЙ--------------------------
-
-digitalWriteF(uint8_t pin, uint8_t val){
-  uint8_t bit = digitalPinToBitMask(pin);
-  uint8_t port = digitalPinToPort(pin);
-  volatile uint8_t *out;
-
-  out = portOutputRegister(port);
-
-  if (val == LOW) {
-    *out &= ~bit;
-  } else {
-    *out |= bit;
-  }  
-}
